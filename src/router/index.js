2902cea0b29dbe3f740fe26bd459d85e51900145
iMocco <taoxin167@gmail.com>
2017-07-13 11:45:44 +0800

    改为按需加载页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/views/index'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: index
-    }
-  ]
+	routes: [{
+		path: '/',
+		component: (resolve) => require(['@/views/index.vue'], resolve),
+		name: 'index'
+	}]
 })
